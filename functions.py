b047fedc93fd96f3bdcc69af16f7366b17eac5d5
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-06-25 12:43:50 +0530

    exeperi_1

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     """" Read the text file and returns the list of
         to-do items
         """
@@ -7,7 +8,7 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """" Write the to-do item list in the text file   """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
