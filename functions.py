84cc13eb8ac975eeb743bd5223720e0fca81ae31
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-06-26 14:34:27 +0530

    initial commit

--- functions.py
+++ functions.py
@@ -8,6 +8,7 @@ def get_todos(filepath=FILEPATH):
     return todos_local
 
 
+
 def write_todos(todos_arg, filepath=FILEPATH):
     """" Write the to-do item list in the text file   """
     with open(filepath, 'w') as file:
